038ab23d84fd57c1ae00869cf1043ed6ace12c9e
Sridhar000085 <srisri72775@gmail.com>
2026-07-02 11:27:01 +0530

    Correct isPrime function and input parsing
    Fix prime number checking logic and improve input handling.

--- Prime.js
+++ Prime.js
@@ -4,8 +4,23 @@
  * @param {number} n - The number to check.
  * @returns {boolean} True if the number is prime, false otherwise.
  */
+// const n=parseint();
+const n = parseint(process.argv[2]);
 function isPrime(n) {
-    // type code here
+    if (num <=1) return false;
+
+for (let i = 2; i <++){
+    if (num % i === 0){
+    return false;
+}
+}
+return true;
 }
 
-module.exports = isPrime;
+if (isprime(n)){
+    console.log("Prime Number");
+}else{
+    console.log("Not Prime Number");
+}
+
+module.exports=isPrime;
